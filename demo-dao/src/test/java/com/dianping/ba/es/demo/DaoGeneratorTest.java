544f821fc926f78179ef95e40affd231565f4961
jing.zhang.sh2 <jing.zhang.sh2@dianping.com>
2017-03-28 15:36:30 +0800

    add code gen.

---
+++ demo-dao/src/test/java/com/dianping/ba/es/demo/DaoGeneratorTest.java
@@ -0,0 +1,18 @@
+package com.dianping.ba.es.demo;
+
+import com.dianping.ba.es.demo.dao.AccountDao;
+import com.dianping.ba.es.demo.dao.po.AccountPO;
+import com.sankuai.meituan.mybatis.generator.plugins.plugins.ExecuteGenerator;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by qianyuhang on 7/15/15.
+ */
+public class DaoGeneratorTest extends TestBase {
+
+    @Test
+    public void query(){
+
+    }
+}
